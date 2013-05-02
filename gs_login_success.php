6810459b69a3df92637d3eca58b557325a09c885
Mikey <mikey_w@gwmail.gwu.edu>
2013-05-02 12:37:00 -0400

    WOAHHH
    These are the current files that we have on cobweb

--- gs_login_success.php
+++ gs_login_success.php
@@ -1,3 +1,13 @@
+<?php
+
+include('../../login.php');
+
+if($_SESSION['gs_auth'] == false){
+   header('Location: index.php?view=universal_login');
+}
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
